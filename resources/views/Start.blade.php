a0c480632b57ed0bad199bb4b5319e5f013258c5
darkviolet21 <m.irfan0205@gmail.com>
2023-01-16 10:46:35 +0700

    update dokumen

--- resources/views/Start.blade.php
+++ resources/views/Start.blade.php
@@ -1,152 +1,142 @@
 @extends('layouts.master')
 @section('content')
- <link rel="stylesheet" href="Start.css">
- <section class="wrapper">
-    <div class="container-fostrap">
-        <div>
-            <h1 class="heading">
-                Berkas CV SATRIA MEDIA UTAMA
-            </h1>
-        </div>
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-4">
-                        <div class="card">
-                            <a class="img-card" href="http://www.fostrap.com/2016/03/bootstrap-3-carousel-fade-effect.html">
-                            <img src="images/Nip.png" />
-                          </a>
-                            <div class="card-content">
-                                <h4 class="card-title">
-                                    <a href="file:///D:/KP/Folder%20Berkas/SIB.pdf"> NIB berkas
-                                  </a>
-                                </h4>
-                                <p class="">
-                                    Identitas pelaku usaha yang diterbitkan oleh lembaga OSS....
-                                </p>
-                            </div>
-                            <div class="card-read-more">
-                                <a href="https://drive.google.com/file/d/1Yc8RLS8V67P64X5nMuHQOKc7n5MkhN2o/view?usp=sharing" class="btn btn-link btn-block">
-                                    Read More
+    <link rel="stylesheet" href="Start.css">
+    <section class="wrapper">
+        <div class="container-fostrap">
+            <div>
+                <h1 class="heading">
+                    Berkas CV SATRIA MEDIA UTAMA
+                </h1>
+            </div>
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card"
+                                    href="https://drive.google.com/file/d/1Yc8RLS8V67P64X5nMuHQOKc7n5MkhN2o/view?usp=sharing">
+                                    <img src="images/Nip.png" />
                                 </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a
+                                            href="https://drive.google.com/file/d/1Yc8RLS8V67P64X5nMuHQOKc7n5MkhN2o/view?usp=sharing">
+                                            NIB berkas
+                                        </a>
+                                    </h4>
+
+                                </div>
+                                <div class="card-read-more">
+                                    <a href="https://drive.google.com/file/d/1Yc8RLS8V67P64X5nMuHQOKc7n5MkhN2o/view?usp=sharing"
+                                        class="btn btn-link btn-block">
+                                        Read More
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-4">
-                        <div class="card">
-                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
-                            <img src="images/siu.png" />
-                          </a>
-                            <div class="card-content">
-                                <h4 class="card-title">
-                                    <a href="file:///D:/KP/Folder%20Berkas/SIB.pdf"> Surat Izin Usaha Perdanggan
-                                  </a>
-                                </h4>
-                                <p class="">
-                                    Surat Izin Perdanggan ....
-                                </p>
-                            </div>
-                            <div class="card-read-more">
-                                <a href="https://drive.google.com/file/d/1CjarE_fA333NiSXycCGm_bwtGuGz36dg/view?usp=sharing" class="btn btn-link btn-block">
-                                    Read More
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card"
+                                    href="https://drive.google.com/file/d/1CjarE_fA333NiSXycCGm_bwtGuGz36dg/view?usp=sharing">
+                                    <img src="images/siu.png" />
                                 </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a
+                                            href="https://drive.google.com/file/d/1CjarE_fA333NiSXycCGm_bwtGuGz36dg/view?usp=sharing">
+                                            Surat Izin Usaha Perdanggan
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="card-read-more">
+                                    <a href="https://drive.google.com/file/d/1CjarE_fA333NiSXycCGm_bwtGuGz36dg/view?usp=sharing"
+                                        class="btn btn-link btn-block">
+                                        Read More
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-4">
-                        <div class="card">
-                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
-                            <img src="images/kementerian.png" />
-                          </a>
-                            <div class="card-content">
-                                <h4 class="card-title">
-                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Kementerian
-                                  </a>
-                                </h4>
-                                <p class="">
-                                    tutorials button hover animation, although very much a hover button is very beauti...
-                                </p>
-                            </div>
-                            <div class="card-read-more">
-                                <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html" class="btn btn-link btn-block">
-                                    Read More
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="">
+                                    <img src="images/kementerian.png" />
                                 </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="">Kementerian
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="card-read-more">
+                                    <a href="" class="btn btn-link btn-block">
+                                        Read More
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-4">
-                        <div class="card">
-                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
-                            <img src="images/npwp.png" />
-                          </a>
-                            <div class="card-content">
-                                <h4 class="card-title">
-                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">NPWP
-                                  </a>
-                                </h4>
-                                <p class="">
-                                    tutorials button hover animation, although very much a hover button is very beauti...
-                                </p>
-                            </div>
-                            <div class="card-read-more">
-                                <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html" class="btn btn-link btn-block">
-                                    Read More
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="">
+                                    <img src="images/npwp.png" />
                                 </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="">NPWP
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="card-read-more">
+                                    <a href="" class="btn btn-link btn-block">
+                                        Read More
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                     <div class="col-xs-12 col-sm-4">
-                        <div class="card">
-                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
-                            <img src="images/kswp.png" />
-                          </a>
-                            <div class="card-content">
-                                <h4 class="card-title">
-                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html"> KSWP
-                                  </a>
-                                </h4>
-                                <p class="">
-                                    tutorials button hover animation, although very much a hover button is very beauti...
-                                </p>
-                            </div>
-                            <div class="card-read-more">
-                                <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html" class="btn btn-link btn-block">
-                                    Read More
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="">
+                                    <img src="images/kswp.png" />
                                 </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="">
+                                            KSWP
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="card-read-more">
+                                    <a href="" class="btn btn-link btn-block">
+                                        Read More
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-4">
-                        <div class="card">
-                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
-                            <img src="images/sk.png" />
-                          </a>
-                            <div class="card-content">
-                                <h4 class="card-title">
-                                    <a href="file:///D:/KP/Folder%20Berkas/Surat%20Keterangan%20Lurah.pdf">Surat Domisili Tempat
-                                  </a>
-                                </h4>
-                                <p class="">
-                                    tutorials button hover animation, although very much a hover button is very beauti...
-                                </p>
-                            </div>
-                            <div class="card-read-more">
-                                <a href="https://drive.google.com/file/d/1tAmK_rQuUs2WghBs9lKD0fnr3vYBpVjC/view?usp=sharing" class="btn btn-link btn-block">
-                                    Read More
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card"
+                                    href="https://drive.google.com/file/d/1tAmK_rQuUs2WghBs9lKD0fnr3vYBpVjC/view?usp=sharing">
+                                    <img src="images/sk.png" />
                                 </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a
+                                            href="https://drive.google.com/file/d/1tAmK_rQuUs2WghBs9lKD0fnr3vYBpVjC/view?usp=sharing">Surat
+                                            Domisili Tempat
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="card-read-more">
+                                    <a href="https://drive.google.com/file/d/1tAmK_rQuUs2WghBs9lKD0fnr3vYBpVjC/view?usp=sharing"
+                                        class="btn btn-link btn-block">
+                                        Read More
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    </div>
-</section>
-
-
-
-
-
-
+        </div>
+    </section>
 @endsection
